8a2a35be685a0e6391ba94239026a179a4947a65
mostconst <113008093+mostconst@users.noreply.github.com>
2022-11-25 11:03:05 +0300

    Create synchronized.h

---
+++ problem21/synchronized.h
@@ -0,0 +1,23 @@
+#include <mutex>
+
+template <typename T>
+class Synchronized {
+public:
+    explicit Synchronized(T initial = T())
+        : value(std::move(initial))
+    {
+    }
+
+    struct Access {
+        T& ref_to_value;
+        std::lock_guard<std::mutex> guard;
+    };
+
+    Access GetAccess() {
+        return { value, std::lock_guard(m) };
+    }
+
+private:
+    T value;
+    std::mutex m;
+};
